201a2575c1324578158a8f2d0808547bf9553a16
MatheusS4nt0s <matheus.s.santos1@outlook.com>
2024-10-28 20:11:29 -0300

    codigo finalizado para mostrar o nome do eroi e seu level de acordo com o seu xp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// teste agora já era
+let eroi = "Mestre dos Magos";
+let xp = 5001;
+let level = "";
+
+if (xp < 1000) {
+    level = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    level = "Bronze";
+} else if (xp > 2000 && xp <= 5000) {
+    level = "Prata";
+} else if (xp > 5000 && xp <= 7000) {
+    level = "Ouro";
+} else if (xp > 7000 && xp <= 8000) {
+    level = "Platina";
+} else if (xp > 8000 && xp <= 9000) {
+    level = "Ascendente";
+} else if (xp > 9000 && xp <= 10000) {
+    level = "Imortal";
+} else if (xp > 10000) {
+    level = "Radiante";
+}
+
+console.log("O Herói de nome " + eroi + " está no nível de " + level);
+
+
